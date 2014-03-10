be7a6a51e8a2a09d22254a8e8cd6e76183d9757e
James Walker <walkah@walkah.net>
2014-03-10 10:45:53 -0400

    Rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,13 @@ description      'Installs/Configures Drupal'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports "debian"
-supports "ubuntu"
+supports 'debian'
+supports 'ubuntu'
 
-depends "apache2",            "~> 1.8.14"
-depends "apt",                "~> 2.3.4"
-depends "build-essential",    "~> 1.4.2"
-depends "drush"
-depends "git",                "~> 2.9.0"
-depends "mysql",              "~> 4.0.20"
-depends "php",                "~> 1.3.12"
+depends 'apache2',            '~> 1.8.14'
+depends 'apt',                '~> 2.3.4'
+depends 'build-essential',    '~> 1.4.2'
+depends 'drush'
+depends 'git',                '~> 2.9.0'
+depends 'mysql',              '~> 4.0.20'
+depends 'php',                '~> 1.3.12'
